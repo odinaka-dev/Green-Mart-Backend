7654530303e6786d07acab4a374ddb3919a55e58
odinaka-dev <uchechukwupeter22@gmail.com>
2026-05-27 15:23:13 +0100

    Fixes and inclusion of guestId for unregistered users

---
+++ src/model/guest.model.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const guestSchema = new mongoose.Schema(
+  {
+    guestId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    cart: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: Number,
+      },
+    ],
+    favorites: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+    expiresAt: {
+      type: Date,
+      default: () => Date.now() + 7 * 24 * 60 * 60 * 1000,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Automatically remove expired guest documents from MongoDB
+guestSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+const Guest = mongoose.model("Guest", guestSchema);
+export default Guest;
